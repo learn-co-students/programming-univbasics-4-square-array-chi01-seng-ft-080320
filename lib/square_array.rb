41420bc456fdfe4e1652545b10501b7c04341aa2
John Easter <john@owltestprep.com>
2020-07-08 02:20:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(array = [26, 27, 28, 29, 30, 31, 32, 33])
+  counter = 0
+  square_array = []
+  while counter < array.length do
+    square_array.push(array[counter] ** 2)
+    counter += 1
+  end
+  square_array
 end
