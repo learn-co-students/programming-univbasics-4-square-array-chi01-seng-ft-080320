e7361084df11492cacf2d05c373546f8d45f067d
Percy Major <chipercy@hotmail.com>
2020-07-31 07:42:49 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+  
+def square_array(numbers)
+  count = 0
+  numbers = [1,2,3]
+    while count < numbers.length do
+        puts numbers.map { |e| e ** 2 }
+        count += 1
+    end
 end
+
+ 
